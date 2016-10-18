b435877565182ea43ad21be219cd9b7c8aaed5e0
Joseph Houser <josephghouser@gmail.com>
2016-10-18 14:00:27 -0500

    javagram somewhat working 10/18

--- java-track/assignments/javagram/Javagram.java
+++ java-track/assignments/javagram/Javagram.java
@@ -16,6 +16,8 @@ public class Javagram {
 		String relPath;
 		Picture picture = null;
 		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to Javagram, our homemade image filter program.");
+		System.out.println("First let's select an image.");
 		
 		// prompt user for image to filter and validate input
 		do {
@@ -25,7 +27,7 @@ public class Javagram {
 			// try to open the file
 			try {
 				
-				System.out.println("Image path (relative to " + dir + "):");
+				System.out.println("Please provide an image path (relative to " + dir + "):");
 				relPath = in.next();
 				
 				/*String[] relPathParts = relPath.split(File.separator);
@@ -42,10 +44,32 @@ public class Javagram {
 		} while(picture == null);
 		
 		// TODO - prompt user for filter and validate input
+		//int chosenInt = displayFilterMenu(in);
+		
+		System.out.println("Cool. Now that we have an image here are your filter options.");
+		System.out.println("1. BlueFilter");
+		System.out.println("2. RedFilter");
+		System.out.println("3. TBDFilter");
+		System.out.println("Please select a filter option");
+		
+		boolean valid_input = false;
+		
+		//i need to instantiate variable outside of try block, but can't do that without making it a filter type. 
+		Filter filter = new BlueFilter();
+		do {
+		int selection = in.nextInt();
 		
 		// TODO - pass filter ID int to getFilter, and get an instance of Filter back 
-		Filter filter = getFilter();			
-
+		try {
+			filter = getFilter(selection);
+			valid_input = true;
+			}
+		catch (IllegalArgumentException e) 
+			{
+			System.out.println("Invalid selection. Try again.");
+			}
+		} while (valid_input == false);
+		
 		// filter and display image
 		Picture processed = filter.process(picture);
 		processed.show();
@@ -71,13 +95,61 @@ public class Javagram {
 		in.close();
 	}
 	
-	// TODO - refactor this method to accept an int parameter, and return an instance of the Filter interface
-	// TODO - refactor this method to thrown an exception if the int doesn't correspond to a filter
+	//refactor this method to accept an int parameter, and return an instance of the Filter interface
+	//refactor this method to thrown an exception if the int doesn't correspond to a filter
 	private static Filter getFilter(int fNumber) {
-		
-		// TODO - create some more filters, and add logic to return the appropriate one
-		return new BlueFilter();
-		
-	}
+		if (fNumber == 1) {
+				return new BlueFilter();
+			}
+			else if (fNumber == 2) {
+				return new RedFilter();
+			}
+			else throw new IllegalArgumentException();
+		}
+	
+	
+	//	catch (IllegalArgumentException e) {
+	//		System.out.println("That didn't work right.");
+	//	}
+	//	} while (valid_input == false);
+		/*		switch(fNumber){
+		case 1: 
+			filter = new BlueFilter();
+			return filter;
+		case 2:
+			filter = new RedFilter();
+			return filter;
+		//default:
+		//	throw e;
+		}*/
+	
 
+//	private static int displayFilterMenu(Scanner in) {
+		
+	/*//menu for user to choose from
+	System.out.println("Cool. Now that we have an image here are your filter options.");
+	System.out.println("1. BlueFilter");
+	System.out.println("2. RedFilter");
+	System.out.println("3. TBDFilter");
+	System.out.println("Please select a filter option");
+	
+	int selection = in.nextInt();
+	boolean valid_input = false;
+	do {
+		try {
+			getFilter(selection);
+			valid_input = true;
+		}
+		catch (IllegalArgumentException ex) {
+			System.err.println("Illegal integer:" + ex.getMessage());
+			System.out.println("Invalid selection. Please try again.");
+		}
+	}
+	while (valid_input ==  false);
+	
+	return selection;
+	
+	
+	
+	}*/
 }
