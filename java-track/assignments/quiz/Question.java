663532843bfacf70f2d9f4398042610d985f95b9
Joseph Houser <josephghouser@gmail.com>
2016-10-17 20:15:53 -0500

    more work 10/17

---
+++ java-track/assignments/quiz/Question.java
@@ -0,0 +1,63 @@
+package quiz;
+
+public class Question {
+
+	public class WriteIn {
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private String qText;
+	private String answer;
+	private int difficulty;
+	
+	public Question (String qText, String answer)
+	{
+		this.qText = qText;
+		this.answer = answer;
+		
+	}
+	
+	public String getQuestion()
+	{
+		return this.qText;
+	}
+	
+	public String getAnswer()
+	{
+		return this.answer;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java-track/assignments/quiz/WriteIn.java
@@ -0,0 +1,5 @@
+package quiz;
+
+public class WriteIn extends Question {
+
+}
